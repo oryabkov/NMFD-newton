2ebe9e476e130cf481c38da9943b6d2c3610f518
aavoyko <aa.voyko@mail.ru>
2026-01-13 20:09:47 +0000

    feat NMFD-newton: refactor more methods in dense_operations_space to use for_each

--- include/nmfd/operations/kernels/dense_vector_space.h
+++ include/nmfd/operations/kernels/dense_vector_space.h
@@ -1,6 +1,7 @@
-#ifndef __VECTOR_SPACE_H__
-#define __VECTOR_SPACE_H__
+#ifndef __NMFD_KERNELS_DENSE_VECTOR_SPACE_H__
+#define __NMFD_KERNELS_DENSE_VECTOR_SPACE_H__
 
+#include <cmath>
 #include <cstddef>
 #include <utility>
 
@@ -10,23 +11,240 @@
  * Backend independent kernels implementing vector operations
  * to run on heterogenous devices.
  ************************************************************/
-
+namespace nmfd
+{
+namespace operations
+{
 namespace kernels
 {
 
-template <class Scalar, class ValueType>
-struct assign_scalar
+template <class Scalar, class VectorType>
+struct check_is_valid_number
 {
-    Scalar s;
-    ValueType* x;
+    const Scalar* x;
+    Scalar* z;
 
     template <class Idx>
     __DEVICE_TAG__ void operator()(const Idx idx)
     {
-        x[idx] = s;
+        z[idx] = std::isfinite(x[idx]);
     }
 };
 
+template <class Scalar, class VectorType>
+struct scalar_prod
+{
+    const Scalar* x;
+    const Scalar* y;
+    Scalar* z;
+
+    template <class Idx>
+    __DEVICE_TAG__ void operator()(const Idx idx)
+    {
+        z[idx] = x[idx] * y[idx];
+    }
+};
+
+template <class Scalar>
+struct assign_scalar
+{
+    Scalar scalar;
+    Scalar* x;
+
+    template <class Idx>
+    __DEVICE_TAG__ void operator()(const Idx idx)
+    {
+        x[idx] = scalar;
+    }
+};
+
+template <class Scalar>
+struct add_mul_scalar
+{
+    Scalar scalar;
+    Scalar mul_x;
+    Scalar* x;
+
+    template <class Idx>
+    __DEVICE_TAG__ void operator()(const Idx idx)
+    {
+        x[idx] = mul_x * x[idx] + scalar;
+    }
+};
+
+template <class Scalar, class VectorType>
+struct norm_inf
+{
+    const Scalar* x;
+    Scalar* z;
+
+    template <class Idx>
+    __DEVICE_TAG__ void operator()(const Idx idx)
+    {
+        z[idx] = std::abs(x[idx]);
+    }
+};
+
+template <class Scalar, class VectorType>
+struct sum
+{
+    const Scalar* x;
+    Scalar* z;
+
+    template <class Idx>
+    __DEVICE_TAG__ void operator()(const Idx idx)
+    {
+        z[idx] = x[idx];
+    }
+};
+
+template <class Scalar, class VectorType>
+struct asum
+{
+    const Scalar* x;
+    Scalar* z;
+
+    template <class Idx>
+    __DEVICE_TAG__ void operator()(const Idx idx)
+    {
+        z[idx] = std::abs(x[idx]);
+    }
+};
+
+template <class Scalar>
+struct assign
+{
+    const Scalar* x;
+    Scalar* z;
+
+    template <class Idx>
+    __DEVICE_TAG__ void operator()(const Idx idx)
+    {
+        z[idx] = x[idx];
+    }
+};
+
+template <class Scalar>
+struct assign_lin_comb
+{
+    Scalar mul_x;
+    const Scalar* x;
+    Scalar mul_y;
+    const Scalar* y;
+    Scalar* z;
+
+    template <class Idx>
+    __DEVICE_TAG__ void operator()(const Idx idx)
+    {
+        z[idx] = mul_x * x[idx] + mul_y * y[idx];
+    }
+};
+
+template <class Scalar>
+struct add_lin_comb
+{
+    Scalar mul_x;
+    const Scalar* x;
+    Scalar mul_y;
+    const Scalar* y;
+    Scalar mul_z;
+    Scalar* z;
+
+    template <class Idx>
+    __DEVICE_TAG__ void operator()(const Idx idx)
+    {
+        z[idx] = mul_x * x[idx] + mul_y * y[idx] + mul_z * z[idx];
+    }
+};
+
+template <class Scalar>
+struct make_abs_copy
+{
+    const Scalar* x;
+    Scalar* y;
+
+    template <class Idx>
+    __DEVICE_TAG__ void operator()(const Idx idx)
+    {
+        y[idx] = std::abs(x[idx]);
+    }
+};
+
+template <class Scalar>
+struct make_abs
+{
+    Scalar* x;
+
+    template <class Idx>
+    __DEVICE_TAG__ void operator()(const Idx idx)
+    {
+        x[idx] = std::abs(x[idx]);
+    }
+};
+
+template <class Scalar>
+struct max_pointwise
+{
+    Scalar sc;
+    const Scalar* x;
+    Scalar* y;
+
+    template <class Idx>
+    __DEVICE_TAG__ void operator()(const Idx idx)
+    {
+        y[idx] = (x[idx] > y[idx]) ? ((x[idx] > sc) ? x[idx] : sc) : ((y[idx] > sc) ? y[idx] : sc);
+    }
+};
+
+template <class Scalar>
+struct min_pointwise
+{
+    Scalar sc;
+    const Scalar* x;
+    Scalar* y;
+
+    template <class Idx>
+    __DEVICE_TAG__ void operator()(const Idx idx)
+    {
+        y[idx] = (x[idx] < y[idx]) ? ((x[idx] < sc) ? x[idx] : sc) : ((y[idx] < sc) ? y[idx] : sc);
+    }
+};
+
+template <class Scalar>
+struct mul_pointwise
+{
+    Scalar mul_x;
+    const Scalar* x;
+    Scalar mul_y;
+    const Scalar* y;
+    Scalar* z;
+
+    template <class Idx>
+    __DEVICE_TAG__ void operator()(const Idx idx)
+    {
+        z[idx] = (mul_x * x[idx]) * (mul_y * y[idx]);
+    }
+};
+
+template <class Scalar>
+struct div_pointwise
+{
+    Scalar mul_x;
+    const Scalar* x;
+    Scalar mul_y;
+    const Scalar* y;
+    Scalar* z;
+
+    template <class Idx>
+    __DEVICE_TAG__ void operator()(const Idx idx)
+    {
+        z[idx] = (mul_x * x[idx]) / (mul_y * y[idx]);
+    }
+};
+
+
 } // namespace kernels
+} // namespace operations
+} // namespace nmfd
 
 #endif
