94b6f44ebc7d9a2f76bbe01d6fa48b19751b53e9
aavoyko <aa.voyko@mail.ru>
2026-02-22 19:13:21 +0000

    feat NMFD-newton: implement traits for scfd array

--- test/operations/scfd_array_traits.h
+++ test/operations/scfd_array_traits.h
@@ -1,39 +1,45 @@
-#ifndef __TEST_STATIC_VECTOR_TRAITS_H__
-#define __TEST_STATIC_VECTOR_TRAITS_H__
+#ifndef __TEST_SCFD_ARRAY_TRAITS_H__
+#define __TEST_SCFD_ARRAY_TRAITS_H__
 
-#include <array>
 #include <cstddef>
+#include <scfd/arrays/array.h>
 
-template <class T, size_t Dim>
-class static_vector_traits
+template <class T, class Memory>
+class scfd_array_traits
 {
 public:
     using scalar_type = T;
-    using vector_type = std::array<T, Dim>;
+    using memory_type = Memory;
+    using vector_type = scfd::arrays::array<scalar_type, memory_type>;
 
 public:
+    scfd_array_traits() = default;
+    explicit scfd_array_traits( size_t size ) : size_( size )
+    {
+    }
+
     void alloc( size_t loc_sz, vector_type &v )
     {
-        // do nothing
+        v.init( loc_sz );
     }
 
     void dealloc( vector_type &v )
     {
-        // do nothing
+        v.free();
     }
 
-    // returns raw pointer to vector first elem
     scalar_type *get_raw_ptr( vector_type &v )
     {
-        return v.data();
+        return v.raw_ptr();
     }
     const scalar_type *get_raw_ptr( const vector_type &v ) const
     {
-        return v.data();
+        return v.raw_ptr();
     }
+
     [[nodiscard]] size_t get_loc_size( const vector_type &v ) const
     {
-        return size_;
+        return v.size();
     }
     [[nodiscard]] size_t size() const
     {
@@ -45,7 +51,7 @@ public:
     }
 
 private:
-    size_t size_{ Dim };
+    size_t size_{ 0 };
 };
 
 #endif
